90996b791d73499a8a44251e53fcff20775e3f0e
caithesai <caithesai123@gmail.com>
2025-10-29 15:33:12 -0700

    Terrain Stuff

--- Texturing/data.cpp
+++ Texturing/data.cpp
@@ -4,7 +4,12 @@
 /// Allowed Files:
 
 ".spp"
+".ssp_painter_lock"
 ".sbsar"
 ".sbs"
 ".png"
 ".jpg"
+
+
+/// Please Utilize Subfolders when pushing textures
+// DO NOT Place FBX or OBJ models in these folders
