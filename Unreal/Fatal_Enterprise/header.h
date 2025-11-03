0f2f1d51b56a8a95faead23c6654154ddb1533fb
caithesai <70312347+caitheai@users.noreply.github.com>
2025-11-03 14:47:34 -0800

    working on inv

---
+++ Unreal/Fatal_Enterprise/header.h
@@ -0,0 +1,13 @@
+/// Fatal Enterprise
+
+/// DO NOT REUPLOAD
+
+/// Credits:
+// - Lead Programmer: Cai Mcdonnell
+// - Lead Producer: Andrew Balaris
+// - Lead Modeler: Jeden Chase
+// - Lead Texture Artist: Sean Gibson
+
+// Direct all questions regarding Usage and Crediting to the following discord
+
+					" caithesai "
